b22f2be7c6308166ee377546e8e275f6e4d46db4
Darrel <darrel@tavis.ca>
2013-07-12 13:07:08 -0400

    Added nuspec file and license

--- Status/Properties/AssemblyInfo.cs
+++ Status/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Status")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tavis.Status")]
+[assembly: AssemblyDescription("application/Status+xml Media Type parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Status")]
+[assembly: AssemblyCompany("Tavis Software Inc.")]
+[assembly: AssemblyProduct("Tavis.Status")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
